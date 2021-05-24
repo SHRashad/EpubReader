440847863d20b75a33bf66e0ff5122f34f5bf483
Rashad <rashad.shirizada@gmail.com>
2021-05-24 14:52:03 +0400

    Test verison

---
+++ EpubReader/EpubReader/EpubReader.h
@@ -0,0 +1,18 @@
+//
+//  EpubReader.h
+//  EpubReader
+//
+//  Created by Rashad Shirizada on 24.05.21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EpubReader.
+FOUNDATION_EXPORT double EpubReaderVersionNumber;
+
+//! Project version string for EpubReader.
+FOUNDATION_EXPORT const unsigned char EpubReaderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EpubReader/PublicHeader.h>
+
+
---
+++ EpubReader/EpubReader/Guide.swift
@@ -0,0 +1,16 @@
+//
+//  Guide.swift
+//  EpubReader
+//
+//  Created by Rashad Shirizada on 24.05.21.
+//
+
+import Foundation
+
+class Guide {
+    
+    
+    init(){
+        print("Hey, i am guide here")
+    }
+}
